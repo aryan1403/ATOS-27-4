5a78cdab7713e5b500a2a5c284846169d1becfc0
Aaryan <aaryan14032006@gmail.com>
2021-12-27 15:50:26 +0530

    meow?

--- meow.java
+++ meow.java
@@ -1,8 +1,32 @@
 public class meow {
     public static void main(String[] args) {
-        // Executes
+        /* // Executes
         System.out.print("Meow Meow"); // print something ??? whatever you write
-        System.out.print("Aaryan"); 
+        System.out.print("Aaryan");  */
+
+        /* // Data Types
+        int a = 4; // any negative and positive value but it can't be decimal
+        int b = 2;
+        System.out.println(a % b); // remainder is zero */
+
+        /* String m = "Meow";
+        String a = "Aaryan";
+
+        char ch = 'a';
+
+
+
+        System.out.println(ch); */
+
+        /* double d = 1.09;
+
+
+        System.out.println(d * 2.91); */
+
+        /* boolean bool = true; // true/false conditional statements
+
+        System.out.println(bool); */
+
 
     }
 }
