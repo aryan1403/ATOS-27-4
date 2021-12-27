06cdd0f157fd1a175ce101dd5a26886ba7bdcd2d
Aaryan <aaryan14032006@gmail.com>
2021-12-27 15:09:36 +0530

    Started coding meow meow

---
+++ meow.java
@@ -0,0 +1,8 @@
+public class meow {
+    public static void main(String[] args) {
+        // Executes
+        System.out.print("Meow Meow"); // print something ??? whatever you write
+        System.out.print("Aaryan"); 
+
+    }
+}
