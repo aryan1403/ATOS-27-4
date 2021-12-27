562708070b79ea1995af854ef55624c62d3cc637
Aaryan <aaryan14032006@gmail.com>
2021-12-27 15:49:56 +0530

    if else done!

---
+++ cmeow.java
@@ -0,0 +1,11 @@
+public class cmeow {
+    public static void main(String[] args) {
+        int a = 1;
+
+        if(a == 0) { // == return us a boolean value (compare something)
+            System.out.println("a = " + a);
+        } else {
+            System.out.println("a is not equal to 0"); // ommmited (ignored)
+        }
+    }
+}
