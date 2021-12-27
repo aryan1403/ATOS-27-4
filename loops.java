1bfe79b77b86f2e88b407837f47a81e703aa90d0
Aaryan <aaryan14032006@gmail.com>
2021-12-27 15:50:15 +0530

    loops done

---
+++ loops.java
@@ -0,0 +1,20 @@
+public class loops {
+    public static void main(String[] args) {
+        // do a work several times
+        /* int counter = 0; // 0
+        while(counter < 5) { // true false
+            System.out.println(counter); // print 0 1 2 3 4
+            counter++; // 1
+        }
+        System.out.println("Done! meww"); */
+
+        /* for (int i = 1; i <= 10; i++) {
+            System.out.println(i); // 1 - 10
+        } */
+        /* boolean start = true;
+        while(start) {
+            System.out.println("Aaryan");
+            start = false;
+        } */
+    }
+}
