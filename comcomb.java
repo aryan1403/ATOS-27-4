1d62626a34382aff76a7234e736c07b3fce63c1e
Aaryan <aaryan14032006@gmail.com>
2021-12-30 10:27:41 +0530

    prime palindrome number

--- comcomb.java
+++ comcomb.java
@@ -1,23 +1,41 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class comcomb {
+    public static boolean isPrime(int num) {
+        boolean prime = true;
+        for (int i = num - 1; i > 1; i--) {
+            if (num % i == 0) {
+                prime = false;
+            }
+        }
+        return prime;
+    }
+
+    public static boolean isPalindrome(int num) {
+        String s = num + "";
+        String r = "";
+        for (int i = s.length() - 1; i >= 0; i--) {
+            r += s.charAt(i);
+        }
+        if (s.equals(r)) {
+            return true;
+        }
+        return false;
+    }
+
     public static void main(String[] args) {
         List<Integer> list = new ArrayList<>();
 
-        list.add(2000);
-        list.add(123);
-        list.add(0);
-        list.add(12);
-        list.add(-10);
+        list.add(10);
+        list.add(11);
+        list.add(2);
+        list.add(111);
 
-        list.sort((a, b) -> {
-            if(a < b) {
-                return -1;
-            }
-            return 0;
-        });
-
-        list.forEach(System.out::println);
+        list.stream().filter(e -> (isPrime(e) && isPalindrome(e)))
+                .collect(Collectors.toList())
+                .forEach(System.out::println);
     }
 }
