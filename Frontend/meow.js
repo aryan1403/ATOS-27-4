fff4d7f14702f0d1234c34c8ecab2b32a948fc27
Aaryan <aaryan14032006@gmail.com>
2022-01-12 15:34:04 +0530

    meowwww

--- Frontend/meow.js
+++ Frontend/meow.js
@@ -1,4 +1,5 @@
 let cartItems = [];
+let counter = 1;
 
 function addToCart() {
   const title = document.getElementById("cardT").innerHTML;
@@ -17,4 +18,12 @@ function addToCart() {
   }
 
   localStorage.setItem("data", JSON.stringify(cartItems));
+}
+
+function increment(count) {
+  document.getElementById(count).innerHTML = "Buy " + ++counter;
+}
+
+function decrement(count) {
+  document.getElementById(count).innerHTML = "Buy " + --counter;
 }
