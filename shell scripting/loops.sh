f1e92d3dcad4bf061f06238a17f3ab8f86020b3d
Aaryan <aaryan14032006@gmail.com>
2022-01-12 10:34:17 +0530

    loops done

---
+++ shell scripting/loops.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+a=0
+while [ "$a" -lt 10 ]    # this is loop1
+do
+   b="$a"
+   while [ "$b" -ge 0 ]  # this is loop2
+   do
+      echo -n "$b "
+      b=`expr $b - 1`
+   done
+   echo
+   a=`expr $a + 1`
+done
