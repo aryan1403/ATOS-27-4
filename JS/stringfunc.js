9c6b28d9825bf2ae853cc4dfbde43a7a1f7c324e
Aaryan <aaryan14032006@gmail.com>
2022-01-13 16:56:13 +0530

    string func done

--- JS/stringfunc.js
+++ JS/stringfunc.js
@@ -1,8 +1,12 @@
-let s = "12";
+/* let s = "12";
 
 let sum = parseInt(s) + 3;
 
 console.log(sum);
 
 let num = 12;
-let s1 = num.toString();
+let s1 = num.toString(); */
+
+let s = "Aaryan";
+
+console.log(s.indexOf("y"));
