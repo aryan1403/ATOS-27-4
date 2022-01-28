f9909d5e6c18b4f05d6d3c78cf66c9ba2f6476d0
Aaryan <aaryan14032006@gmail.com>
2022-01-28 10:04:34 +0530

    unit testing done

--- JDBC/demoproject/src/test/java/com/example/AppTest.java
+++ JDBC/demoproject/src/test/java/com/example/AppTest.java
@@ -1,18 +1,16 @@
 package com.example;
 
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-/**
- * Unit test for simple App.
- */
 class AppTest {
-    /**
-     * Rigorous Test.
-     */
+    
     @Test
-    void testApp() {
-        assertEquals(1, 1);
+    public void add(){
+        assertEquals(9, App.add(4, 5)); // Test Passed
+        assertTrue(App.sub(2, 1) > 0); 
+        assertEquals(10, App.mul(2, 5));
+        assertEquals(null, App.divide(1, 0));
     }
 }
